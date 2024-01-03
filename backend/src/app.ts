8dda19fe804a25f84aa55942562e634818d8d574
amakura-411 <callplus411@gmail.com>
2024-01-03 11:06:38 +0900

    server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,9 @@
-import dbOpen from './frameworks/db/connect'
+import DatabaseOpen from './frameworks/db/connect'
+import ServerStart  from './frameworks/web/server'
 
-// expressのインポート
-import express, { Application, Request, Response } from 'express'
 
-// expressのインスタンス作成
-const app: Application = express()
+  // DB接続
+    DatabaseOpen().catch(console.dir)
 
-// ポート番号
-const port: number = 3000
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello nodemon!')
-})
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
-
-// MongoDBへの接続
-// メソッドが失敗した場合、エラー情報をコンソールに出力する。
-// dbOpen().catch(console.dir);
-// console.dir：オブジェクトのプロパティ構造をツリー形式で表示するため、
-// ネストされたオブジェクトの中身を視覚的に確認するのに役立ちます。
-dbOpen().catch(console.dir)
+  // サーバー起動
+    ServerStart().catch(console.dir)
